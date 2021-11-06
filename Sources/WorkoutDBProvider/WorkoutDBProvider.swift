355984075ba56bb22fbead73674de977527f4b99
Georgios Galatoulas <georgiosgalatoulas@gmail.com>
2021-11-06 19:22:01 +0000

    updated gitignore. changed scope

--- Sources/WorkoutDBProvider/WorkoutDBProvider.swift
+++ Sources/WorkoutDBProvider/WorkoutDBProvider.swift
@@ -1,4 +1,4 @@
-struct WorkoutDBProvider {
+public struct WorkoutDBProvider {
     var text = "Your Workout Database provider"
     var serviceLocator = ServiceLocator(entitiesRepo: RealmEntityRepo())
 }
