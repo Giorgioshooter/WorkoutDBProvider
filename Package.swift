16ec63f6f00771156e46fe7fecd5d2f36ac96014
Georgios Galatoulas <georgiosgalatoulas@gmail.com>
2023-05-15 18:47:55 +0100

    Upgrade Ream lib

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name:"Realm", url: "git@github.com:realm/realm-cocoa.git", .exact("10.15.1"))
+        .package(name:"Realm", url: "git@github.com:realm/realm-cocoa.git", .exact("10.39.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
